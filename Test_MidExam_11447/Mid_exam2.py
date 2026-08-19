d08307edb783fa13ac793f247f4691bcebad146b
FEW <nuttapolporn1234@gmail.com>
2026-08-19 21:41:13 +0700

    new

---
+++ Test_MidExam_11447/Mid_exam2.py
@@ -0,0 +1,11 @@
+# นาย ณัฏฐำพล พรเลิศ 6906021611447
+total = 0
+avg = 0
+for i in range (1,6,1):
+    number = int(input("Enter Number #"+str(i)+":"))
+    total = total + number
+    avg = total/5
+print("="*22)
+print(f"Total = {total}")
+print("="*22)
+print(f"Average = {avg:.2f}\n{"="*22}")
